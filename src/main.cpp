8c9ddd5d6933591dde5c75a6e5c040362f559b02
aahilzaman <aahilzaman@gmail.com>
2026-01-29 11:35:33 +0600

    Edited using meson

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "user.cpp"
-
+#include "aahil.cpp"
 int main(){
     srand((unsigned)time(nullptr));
 	User new_user("001", "Azizul", "123", "haki@iut-dhaka.edu");
 	cout << "Running main.cpp\n";
+	solution s1;
+	if(s1.isvalid("Aahil"))
+	cout<<"YES\n";
+	else cout<<"NO\n";
 }
