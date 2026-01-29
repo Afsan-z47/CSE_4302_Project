1c3d60e807579f96b97780e630876ca0df771168
Afsan <ns_afsan@disroot.org>
2026-01-29 12:43:19 +0600

    FIX: broken user.cpp into header [user.h] and [user.cpp]
    FIX: Did some misc management

--- src/course.cpp
+++ src/course.cpp
@@ -1,22 +1,25 @@
+#include"user.h"
 #include<iostream>
-#include<vector>
+#include <vector>
+
 class Course{
 	std::string courseCode;
 	std::string courseName;
 	std::string studentnumber;
 	//Teacher t1;
-	// vector<Student>s1;
-	// vector<Assessment>a1;
+	std::vector<Student>s1;
+	//std::vector<Assessment>a1;
 public:
 	Course(){}
 	Course(std::string code,std::string sid){
 	courseCode = code;
 	studentnumber = sid;
 	}
-	// void add_students(Student s2){
-	// s1.push_back(s2);
+	
+	void add_students(Student s2){
+	s1.push_back(s2);
 	// }
 	// void add_assessment(Assessment a2){
 	// a1.push_back(a2);
-	// }
+	}
 };
