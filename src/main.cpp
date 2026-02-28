69f630dcb6a84fb7066e39a2a58baf37ae0322e8
Afsan <ns_afsan@disroot.org>
2026-02-28 16:16:53 +0600

    Init schoolSystem

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include "user.h"
+#include "schoolsystem.cpp"
 
 auto main() -> int {
     srand((unsigned)time(nullptr));
 	User new_user("001", "Azizul", "123", "haki@iut-dhaka.edu");
 	std::cout << "Running main.cpp\n";
+	
+	SchoolSystem SYSTEM;
+
 }
