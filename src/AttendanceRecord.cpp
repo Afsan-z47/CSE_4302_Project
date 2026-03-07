4866f7b0fab365338142ee7478d4998fa61ff5ce
Afsan <ns_afsan@disroot.org>
2026-03-07 12:44:33 +0600

    Feat: Added Serialization

--- src/AttendanceRecord.cpp
+++ src/AttendanceRecord.cpp
@@ -1,12 +1,25 @@
 #include "AttendanceRecord.h"
 #include <iostream>
+#include <sstream>
 
 AttendanceRecord::AttendanceRecord(std::string sID, std::string cCode, std::string d, bool status)
-    : studentID(sID), courseCode(cCode), date(d), isPresent(status) {}
+: studentID(sID), courseCode(cCode), date(d), isPresent(status) 
+{}
 
 void AttendanceRecord::displayAttendance() const {
-    std::cout << "Date: " << date <<std::endl;
-       std ::cout<< "Course: " << courseCode <<std::endl;
-            std::cout<<"Student ID: " << studentID <<std::endl;
-              std ::cout<<"Status: " << (isPresent ? "Present" : "Absent") << std::endl;
+	std::cout << "Date: " << date <<std::endl;
+	std ::cout<< "Course: " << courseCode <<std::endl;
+	std::cout<<"Student ID: " << studentID <<std::endl;
+	std ::cout<<"Status: " << (isPresent ? "Present" : "Absent") << std::endl;
+}
+
+// NOTE: ---- Serialise ----
+
+std::string AttendanceRecord::serialise() const {
+	std::ostringstream ss;
+	ss << studentID  << '|'
+		<< courseCode << '|'
+		<< date       << '|'
+		<< (isPresent ? 1 : 0);
+	return ss.str();
 }
